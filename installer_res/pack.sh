f6004762cd00441ae0056b44cb48bd94ab64b268
NathanNeurotic (Ripto) <109461996+NathanNeurotic@users.noreply.github.com>
2025-04-22 04:32:31 -0700

    Update pack.sh

--- installer_res/pack.sh
+++ installer_res/pack.sh
@@ -4,13 +4,12 @@ PKG_DATE=$(date '+[%Y-%m-%d]')
 DATE_FLAT=$(date '+%Y-%m-%d')
 wget https://github.com/israpps/BDMAssault/releases/download/latest/BDMAssault.7z -O BDMAssault.7z
 7z x BDMAssault.7z READY_TO_USE/FreeMcBoot/
-cp ../Changelog.md __base/Changelog.md
 
 for subdir in 1966 1965 1964 1963 1953
 do
     echo "Packing v$subdir..."
-    
-    # For version 1966, rename directory to OSDMenu-Installer-[DATE]
+
+    # Determine output directory and archive name
     if [ "$subdir" = "1966" ]; then
         NEWDIR="OSDMenu-Installer-$DATE_FLAT"
         OUTFILE="../FMCB-1966.7z"
@@ -19,17 +18,25 @@ do
         OUTFILE="../FMCB-$subdir.7z"
     fi
 
+    # Build structure
     cp -r __base/ "$NEWDIR/"
     cp -r "$subdir/INSTALL/" "$NEWDIR/INSTALL/"
+
+    # Replace installer ELF files with renamed versions
+    cp FMCBInstaller.elf "$NEWDIR/OSDMENU-INSTALLER-FAT32.ELF"
+    cp FMCBInstaller_EXFAT.elf "$NEWDIR/OSDMENU-INSTALLER-EXFAT.ELF"
+
+    # Write metadata
     echo "$SHA8" > "$NEWDIR/lang/commit.txt"
     echo "title=FreeMcBoot v$subdir $PKG_DATE" > "$NEWDIR/title.cfg"
-    echo "boot=FMCBInstaller.elf" >> "$NEWDIR/title.cfg"
-    cp FMCBInstaller.elf "$NEWDIR/"
-    cp FMCBInstaller_EXFAT.elf "$NEWDIR/"
-    mkdir -p "$NEWDIR/FMCB_EXFAT/"
-    cp -r READY_TO_USE/FreeMcBoot/SYS-CONF/ "$NEWDIR/FMCB_EXFAT/"
-    cp EXFAT_INSTALL_INSTRUCTIONS.TXT "$NEWDIR/FMCB_EXFAT/INSTRUCTIONS.TXT"
+    echo "boot=OSDMENU-INSTALLER-FAT32.ELF" >> "$NEWDIR/title.cfg"
 
+    # Cleanup
+    rm -rf "$NEWDIR/FMCB_EXFAT/"
+    rm -f "$NEWDIR/README.txt"
+    rm -f "$NEWDIR/Changelog.md"
+    rm -rf "$NEWDIR/Changelog"
+    
     echo "Creating archive $OUTFILE"
     7z a -t7z -r "$OUTFILE" "$NEWDIR"/*
 done
